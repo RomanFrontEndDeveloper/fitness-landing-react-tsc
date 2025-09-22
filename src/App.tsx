7ede828d4b839354079539235a5363f0aae5cfe6
RomanFronrEnd <romariotraveler@gmail.com>
2025-09-22 14:18:38 +0300

    start progekt wiz all bibliotek & npm

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<div className='flex-col justify-center gap-2.5'>
+			<h1 className='text-2xl'>My Fitness Progect</h1>
+			<p className='font-montserrat'>Monserrat</p>
+			<p className='font-dmsans'>Dm sans</p>
+		</div>
+	);
+}
+
+export default App;
